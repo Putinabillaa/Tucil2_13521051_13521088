c38479233019f0c93198d85a06af2155f0864314
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2023-02-27 18:59:02 +0700

    Update ClosestPoint_DnQ.py

--- src/ClosestPoint_DnQ.py
+++ src/ClosestPoint_DnQ.py
@@ -25,27 +25,56 @@ def printPair(pair):
 def closestPair(Points,N):                       # ambil koordinat x, tentukan panjang lebar x yang mungkin
     if (N == 2):
         d = jarak(Points[0], Points[1])
-        addPair(Points[0],Points[1])
-        return d
+        Pair = [Points[0],Points[1]]
+        return d, Pair
     else:
         S1,S2 = split(Points,N)               # bagi menjadi 2
-        d1 = closestPair(S1,int(N/2))
-        d2 = closestPair(S2,int(N/2))
-        d = min(d1,d2)
-        
-        pleft = S1[len(S1)-1]
-        pright = S2[0]
+        d1, Pair1 = closestPair(S1,int(N/2))
+        d2, Pair2 = closestPair(S2,int(N/2))
 
-        if (jarak(pleft,pright) < d):
-            d = jarak(pleft,pright)
+        if (d1 < d2):
+            d = d1
+            Pair = Pair1
+        else:
+            d = d2
+            Pair = Pair2
         
-        return d
+        batas = S1[len(S1)-1][0] + (S1[len(S1)-1][0] + S2[0][0])/2        
+        temp = []
+
+        # cari di kiri
+        i = len(S1)-1
+        while (S1[i][0] > batas - d):
+            temp.append(S1[i][0])
+            i = i - 1
+        # cari di kanan
+        i = 0
+        while (S2[i][0] < batas + d):
+            temp.append(S2[i][0])
+            i = i + 1
+
+        # proses himpunan strip
+        for i in range(len(temp)):
+            for j in range(len(temp)):
+                check = True
+                k = 0
+                while(check and k < 2):
+                    if(abs(temp[i][k] - temp[j][k]) > d):
+                        check = False
+                
+                if (check):
+                    if(jarak(temp[i], temp[j]) < d):
+                        d = jarak(temp[i], temp[j])
+                        Pair = [temp[i], temp[j]]
+
+        return d, Pair
     
 # ALGORITMA UTAMA
-Pair = []
+
 # N = int(input("Masukkan dimensi: "))
 points = [[1,2,3],[3,4,5],[7,6,7],[8,9,10]]
 jumlahPerhitungan = 0
 
-closestPair(points,4)
+d, Pair = closestPair(points,4)
+printPair(d)
 printPair(Pair)
